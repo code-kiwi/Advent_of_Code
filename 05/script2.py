aeb42ee4a88e4a9701ff15884834d60ce1cc0349
mhotting <madric.hottinger.101@gmail.com>
2023-12-08 10:42:29 +0100

    Completes Day05 (script is super slow but works)

--- 05/script2.py
+++ 05/script2.py
@@ -1,6 +1,6 @@
 def get_range(input):
     range = list(map(lambda x: int(x), input.split(" ")))
-    return {"start": range[1], "end": range[1] + range[2] - 1, "start_corr": range[0]}
+    return {"start": range[0], "end": range[0] + range[2] - 1, "start_corr": range[1]}
 
 
 def get_seeds_data(line):
@@ -37,38 +37,49 @@ def get_maps_info(lines):
     return maps
 
 
-def get_corresponding_index(seed, ranges):
+def get_corresponding_index(index, ranges):
     for range in ranges:
-        if seed >= range["start"] and seed <= range["end"]:
-            return range["start_corr"] + seed - range["start"]
-    return seed
+        if index >= range["start"] and index <= range["end"]:
+            return range["start_corr"] + index - range["start"]
+    return index
+
+
+def is_valid_seed(given_ranges, seed):
+    for given_range in given_ranges:
+        if (
+            seed >= given_range["seed_start"]
+            and seed < given_range["seed_start"] + given_range["seed_len"]
+        ):
+            return True
+    return False
 
 
 def main_func():
     with open("./input1.txt", "r") as file:
-        # with open('./testinput.txt', 'r') as file:
+        # with open("./testinput.txt", "r") as file:
         lines = file.readlines()
         seeds = get_seeds_data(lines[0])
         maps = get_maps_info(lines)
         min_loc = -1
-        for seed in seeds:
-            i = 0
-            while i < seed["seed_len"]:
-                soil = get_corresponding_index(
-                    seed["seed_start"] + i, maps["seed-to-soil"]
-                )
-                fert = get_corresponding_index(soil, maps["soil-to-fertilizer"])
-                water = get_corresponding_index(fert, maps["fertilizer-to-water"])
-                light = get_corresponding_index(water, maps["water-to-light"])
-                temp = get_corresponding_index(light, maps["light-to-temperature"])
-                humid = get_corresponding_index(temp, maps["temperature-to-humidity"])
-                loc = get_corresponding_index(humid, maps["humidity-to-location"])
-                if min_loc == -1:
-                    min_loc = loc
-                else:
-                    min_loc = min(min_loc, loc)
-                i += 1
-        print(min_loc)
+
+        # Trying each location one by one until we find a matching seed
+        loc = 0
+        mul = 1
+        while True:
+            if loc % mul == 0:
+                print(loc)
+                mul *= 10
+            humid = get_corresponding_index(loc, maps["humidity-to-location"])
+            temp = get_corresponding_index(humid, maps["temperature-to-humidity"])
+            light = get_corresponding_index(temp, maps["light-to-temperature"])
+            water = get_corresponding_index(light, maps["water-to-light"])
+            fert = get_corresponding_index(water, maps["fertilizer-to-water"])
+            soil = get_corresponding_index(fert, maps["soil-to-fertilizer"])
+            seed = get_corresponding_index(soil, maps["seed-to-soil"])
+            if is_valid_seed(seeds, seed):
+                break
+            loc += 1
+        print(loc)
 
 
 if __name__ == "__main__":
